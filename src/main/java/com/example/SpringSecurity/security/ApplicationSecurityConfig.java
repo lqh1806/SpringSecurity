f497ead03bad2cb50639ba5bcda8661a8775725c
lqhung1806 <lequochung1806@gmail.com>
2022-02-07 21:59:56 +0700

    Test 1

---
+++ src/main/java/com/example/SpringSecurity/security/ApplicationSecurityConfig.java
@@ -0,0 +1,74 @@
+package com.example.SpringSecurity.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static com.example.SpringSecurity.security.ApplicationUserRole.*;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) //Cần để cho phép authority qua các API
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // TODO:
+                .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails user1 = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("1234"))
+//                .roles(STUDENT.name()) //ROLE_STUDENT
+                .authorities(STUDENT.getGrantedAuthorities())
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("admin2")
+                .password(passwordEncoder.encode("12345"))
+//                .roles(ADMIN.name()) //ROLE_ADMIN
+                .authorities(ADMIN.getGrantedAuthorities())
+                .build();
+
+        UserDetails user3 = User.builder()
+                .username("admin3")
+                .password(passwordEncoder.encode("123456"))
+//                .roles(ADMINTRAINEE.name()) //ROLE_ADMINTRAINEE
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
+                .build();
+
+
+        return new InMemoryUserDetailsManager(
+                user1, user2, user3
+        );
+    }
+}
---
+++ src/main/java/com/example/SpringSecurity/student/Student.java
@@ -0,0 +1,29 @@
+package com.example.SpringSecurity.student;
+
+public class Student {
+
+    private final Integer id;
+    private final String studentName;
+
+    public Student(Integer id,
+                   String studentName) {
+        this.id = id;
+        this.studentName = studentName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", studentName='" + studentName + '\'' +
+                '}';
+    }
+}
